3f7af8290906b739a4c58acf5d7ae7dcff4df914
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-02-27 01:12:27 +0200

    fixed

--- Demon/ChangeMatrix/ChangeMatrix/Program.cs
+++ Demon/ChangeMatrix/ChangeMatrix/Program.cs
@@ -2,100 +2,102 @@
 
 namespace ChangeMatrix
 {
-    class Matrix
-    {
-        private int[,] privateMatrix;
+	class Matrix
+	{
+		private int[,] privateMatrix;
 
-        public Matrix()
-        {
-            privateMatrix = new int[4, 4];
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    privateMatrix[j, i] = 0;
-                }
-            }
-        }
+		public Matrix()
+		{
+			int someValue = 10;
+			privateMatrix = new int[4, 4];
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					privateMatrix[i, j] = someValue++;
+				}
+			}
+		}
 
-        //Method changeSecondColumn
-        public void ChangeSecondColumn(int value)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                privateMatrix[1, i] = value;
-            }
-        }
+		//Method changeSecondColumn
+		public void ChangeSecondColumn(int value)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				privateMatrix[1, i] = value;
+			}
+		}
 
-        //Method RotateMatrix
-        public void RotateMatrix()
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                for (int i = 0; i < j; i++)
-                {
-                    int rotate = privateMatrix[i, j];
-                    privateMatrix[j, 3-i] = privateMatrix[i, j];
-                }
-            }
+		//Method RotateMatrix
+		public void RotateMatrix()
+		{
+			var matrixSize = privateMatrix.GetLength(0);
+			var tempMatrix = new int[matrixSize, matrixSize];
+			for (var i = 0; i < matrixSize; i++)
+			{
+				for (var j = 0; j < matrixSize; j++)
+				{
+					tempMatrix[j, matrixSize - 1 - i] = privateMatrix[i, j];
+				}
+			}
+			privateMatrix = tempMatrix;
+		}
+		//Display matrix
+		public void DisplayMatrix()
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					Console.Write(privateMatrix[i,j] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
 
-        }
-        //Display matrix
-        public void DisplayMatrix()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write(privateMatrix[j, i]+" ");                
-                }
-                Console.WriteLine();
-            }
-        }
-    }
+	//for user
+	class User
+	{
+		static void Main()
+		{
+			//create matrix
+			Matrix newMatrix = new Matrix();
 
-    //for user
-    class User
-    {
-        static void Main()
-        {
-            //create matrix
-            Matrix newMatrix = new Matrix();
+			//display default matrix
+			newMatrix.DisplayMatrix();
 
-            //display default matrix
-            newMatrix.DisplayMatrix();
+			//Request
+			int number;
+			Console.WriteLine("\nEnter the number by which to replace the second column of the matrix\n" + "Number: ");
+			string value = Console.ReadLine();
+			number = Convert.ToInt16(value);
 
-            //Request
-            int number;
-            Console.WriteLine("\nEnter the number by which to replace the second column of the matrix\n" + "Number: ");
-            string value = Console.ReadLine();
-            number = Convert.ToInt16(value);
+			//Change column
+			newMatrix.ChangeSecondColumn(number);
 
-            //Change column
-            newMatrix.ChangeSecondColumn(number);
+			//Display matrix after change column
+			newMatrix.DisplayMatrix();
 
-            //Display matrix after change column
-            newMatrix.DisplayMatrix();
+			//Rotate matrix
+			Console.WriteLine("\nRotate matrix? yes/no");
+			string answer = Console.ReadLine().ToLower();
+			switch (answer)
+			{
+				case "yes":
+					{
+						newMatrix.RotateMatrix();
+						newMatrix.DisplayMatrix();
+						break;
+					}
 
-            //Rotate matrix
-            Console.WriteLine("\nRotate matrix? yes/no");
-            string answer = Console.ReadLine().ToLower();
-            switch (answer)
-            {
-                case "yes":
-                    {
-                        newMatrix.RotateMatrix();
-                        newMatrix.DisplayMatrix();
-                        break;
-                    }
-
-                case "no":
-                    {
-                        Console.WriteLine("\nPress any key fo restart program");
-                        string anykey = Console.ReadLine();
-                        break;
-                    }
-            }
-        }
-    }
+				case "no":
+					{
+						Console.WriteLine("\nPress any key fo restart program");
+						string anykey = Console.ReadLine();
+						break;
+					}
+			}
+		}
+	}
 }
--- Seel/changeMatrix/changeMatrix/Program.cs
+++ Seel/changeMatrix/changeMatrix/Program.cs
@@ -3,153 +3,142 @@
 namespace changeMatrix
 {
 
-    class Matrix
-    {
-        private int[,] priv_matrix;
+	class Matrix
+	{
+		private int[,] privateMatrix;
 
-        public Matrix(int size)
-        {
-            priv_matrix = new int[size, size];
-            for (int i = 0; i < priv_matrix.GetLength(0); ++i)
-            {
-                for (int j = 0; j < priv_matrix.GetLength(1); ++j)
-                {
-                    priv_matrix[i, j] = 0;
-                }
-            };
-        }
-        public Matrix()
-        {
-            priv_matrix = new int[4, 4];
-            for (int i = 0; i < priv_matrix.GetLength(0); ++i)
-            {
-                for (int j = 0; j < priv_matrix.GetLength(1); ++j)
-                {
-                    priv_matrix[i, j] = 0;
-                }
-            };
-        }
+		public Matrix() : this(4)
+		{
+		}
 
-        //Method that changes values in every index of second column with value entered as a parameter
-        public void ChangeColumn(int value)
-        {
-            for (int i = 0; i < priv_matrix.GetLength(1); i++)
-            {
-                priv_matrix[i, 1] = value;
-            };
-        }
+		public Matrix(int size)
+		{
+			var valueDefault = 10;
+			privateMatrix = new int[size, size];
+			for (var i = 0; i < privateMatrix.GetLength(0); ++i)
+			{
+				for (var j = 0; j < privateMatrix.GetLength(1); ++j)
+				{
+					privateMatrix[i, j] = valueDefault++;
+				}
+			};
+		}
 
-        //Method overloading, customisable column number
-        public void ChangeColumn(int value, int column_number)
-        {
-            for (int i = 0; i < priv_matrix.GetLength(1); i++)
-            {
-                priv_matrix[i, column_number - 1] = value;
-            };
-        }
+		//Method that changes values in every index of second column with value entered as a parameter
+		public void ChangeColumn(int value)
+		{
+			ChangeColumn(value, 1);
+		}
 
-        //Method that swaps values of columns and rows of matrix
-        public void Rotate()
-        {
-            for (int i = 1; i < priv_matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < i; j++)
-                {
-                    int temp = priv_matrix[i, j];
-                    priv_matrix[i, j] = priv_matrix[j, i];
-                    priv_matrix[j, i] = temp;
-                }
-            };
-        }
+		//Method overloading, customisable column number
+		public void ChangeColumn(int value, int columnNumber)
+		{
+			for (var i = 0; i < privateMatrix.GetLength(1); i++)
+			{
+				privateMatrix[i, columnNumber] = value;
+			};
+		}
 
-        //Method to display matrix content
-        public void DisplayMatrix()
-        {
-            for (int i = 0; i < priv_matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < priv_matrix.GetLength(1); j++)
-                {
-                    Console.Write(priv_matrix[i, j] + " ");
-                }
-                Console.WriteLine();
-            };
-            Console.WriteLine();
-        }
+		//Method that swaps values of columns and rows of matrix
+		public void Rotate()
+		{
+			var matrixSize = privateMatrix.GetLength(0);
+			var tempMatrix = new int[matrixSize, matrixSize];
+			for (var i = 0; i < matrixSize; i++)
+			{
+				for (var j = 0; j < matrixSize; j++)
+				{
+					tempMatrix[j, matrixSize- 1 - i] = privateMatrix[i, j];
+				}
+			}
+			privateMatrix = tempMatrix;
+		}
 
-    }
+		//Method to display matrix content
+		public void DisplayMatrix()
+		{
+			for (var i = 0; i < privateMatrix.GetLength(0); i++)
+			{
+				for (var j = 0; j < privateMatrix.GetLength(1); j++)
+				{
+					Console.Write(privateMatrix[i, j] + " ");
+				}
+				Console.WriteLine();
+			};
+			Console.WriteLine();
+		}
 
-    class UserControl
-    {
-        public UserControl()
-        {
-            Console.WriteLine("Hi! This program can do some cool stuff with a square matrix of integers.\n\n" +
-                "To create new square matrix of custom size enter: 'New'\n" +
-                "To change any column of that matrix to specific value enter: 'Change column'\n" +
-                "To swap columns and rows of the matrix enter: 'Swap'\n" +
-                "To finish fun enter: 'End'\n"+
-                "To see list of commands again enter: 'Help'\n");
-            string command = "";
-            var newMatrix = new Matrix();
+	}
 
+	class Program
+	{
+		static void Main()
+		{
+			UserControl();
+		}
 
-            while (command != "end")
-            {
-                switch (command)
-                {
+		public static void UserControl()
+		{
+			Console.WriteLine("Hi! This program can do some cool stuff with a square matrix of integers.\n\n" +
+				"To create new square matrix of custom size enter: 'New'\n" +
+				"To change any column of that matrix to specific value enter: 'Change column'\n" +
+				"To swap columns and rows of the matrix enter: 'Swap'\n" +
+				"To finish fun enter: 'End'\n" +
+				"To see list of commands again enter: 'Help'\n");
+			string command = string.Empty;
+			var newMatrix = new Matrix();
 
-                    case "new":
-                        {
-                            Console.WriteLine("Cool! What size of the matrix do we need?");
-                            int matrixSize = Int32.Parse(Console.ReadLine());
-                            newMatrix = new Matrix(matrixSize);
-                            newMatrix.DisplayMatrix();
-                            command = null;
-                            break;
-                        }
-                    case "change column":
-                        {
-                            Console.WriteLine("What column do you want to change?");
-                            int columnNumber = Int32.Parse(Console.ReadLine());
-                            Console.WriteLine("What new value we want in "+columnNumber+" column after change?");
-                            int valueNew = Int32.Parse(Console.ReadLine());
-                            newMatrix.ChangeColumn(valueNew, columnNumber);
-                            newMatrix.DisplayMatrix();
-                            command = null;
-                            break;
-                        }
-                    case "swap":
-                        {
-                            newMatrix.Rotate();
-                            newMatrix.DisplayMatrix();
-                            command = null;
-                            break;
-                        }
-                    case "help":
-                        {
-                            Console.WriteLine("To create new square matrix of custom size enter: 'New'\n" +
-                                            "To change any column of that matrix to specific value enter: 'Change column'\n" +
-                                            "To swap columns and rows of the matrix enter: 'Swap'\n" +
-                                            "To finish fun enter: 'End'\n" +
-                                            "To see list of commands again enter: 'Help'\n");
-                            command = null;
-                            break;
-                        }
-                    default:
-                        {
-                            command = Console.ReadLine().ToLower();
-                            break;
-                        }
-                }
-            }
-
-        }
-
-        class Program
-        {
-            static void Main()
-            {
-                new UserControl();
-            }
-        }
-    }
+			while (command != "end")
+			{
+				switch (command)
+				{
+					case "new":
+						{
+							Console.WriteLine("Cool! What size of the matrix do we need?");
+							int matrixSize;
+							if (Int32.TryParse(Console.ReadLine(), out matrixSize) && matrixSize >= 0)
+							{
+								newMatrix = new Matrix(matrixSize);
+								newMatrix.DisplayMatrix();
+							}
+							command = string.Empty;
+							break;
+						}
+					case "change column":
+						{
+							Console.WriteLine("What column do you want to change?");
+							int columnNumber = Int32.Parse(Console.ReadLine());
+							Console.WriteLine("What new value we want in " + columnNumber + " column after change?");
+							int valueNew = Int32.Parse(Console.ReadLine());
+							newMatrix.ChangeColumn(valueNew, columnNumber - 1);
+							newMatrix.DisplayMatrix();
+							command = string.Empty;
+							break;
+						}
+					case "swap":
+						{
+							newMatrix.Rotate();
+							newMatrix.DisplayMatrix();
+							command = string.Empty;
+							break;
+						}
+					case "help":
+						{
+							Console.WriteLine("To create new square matrix of custom size enter: 'New'\n" +
+											"To change any column of that matrix to specific value enter: 'Change column'\n" +
+											"To swap columns and rows of the matrix enter: 'Swap'\n" +
+											"To finish fun enter: 'End'\n" +
+											"To see list of commands again enter: 'Help'\n");
+							command = string.Empty;
+							break;
+						}
+					default:
+						{
+							command = Console.ReadLine().ToLower();
+							break;
+						}
+				}
+			}
+		}
+	}
 }
