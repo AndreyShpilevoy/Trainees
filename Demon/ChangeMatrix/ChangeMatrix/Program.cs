d58c121cbf07bcafd3239453355827d3814a03e2
GRAFELPC\User <adov.demon@yandex.ru>
2017-02-27 00:33:26 +0300

    this shit don`t work

---
+++ Demon/ChangeMatrix/ChangeMatrix/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ChangeMatrix
+{
+    class Matrix
+    {
+        private int[,] privateMatrix;
+
+        public Matrix()
+        {
+            privateMatrix = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    privateMatrix[j, i] = 0;
+                }
+            }
+        }
+
+        //Method changeSecondColumn
+        public void ChangeSecondColumn(int value)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                privateMatrix[1, i] = value;
+            }
+        }
+
+        //Method RotateMatrix
+        public void RotateMatrix()
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    privateMatrix[j, i] = privateMatrix[i, j];
+                }
+            }
+
+        }
+        //Display matrix
+        public void DisplayMatrix()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(privateMatrix[j, i]+" ");                
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    //for user
+    class User
+    {
+        static void Main()
+        {
+            //create matrix
+            Matrix newMatrix = new Matrix();
+
+            //display default matrix
+            newMatrix.DisplayMatrix();
+
+            //Request
+            int number;
+            Console.WriteLine("\nEnter the number by which to replace the second column of the matrix\n" + "Number: ");
+            string value = Console.ReadLine();
+            number = Convert.ToInt16(value);
+
+            //Change column
+            newMatrix.ChangeSecondColumn(number);
+
+            //Display matrix after change column
+            newMatrix.DisplayMatrix();
+
+            //Rotate matrix
+            Console.WriteLine("\nRotate matrix? yes/no");
+            string answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "yes":
+                    {
+                        newMatrix.RotateMatrix();
+                        newMatrix.DisplayMatrix();
+                        break;
+                    }
+
+                case "no":
+                    {
+                        Console.WriteLine("\nPress any key fo restart program");
+                        string anykey = Console.ReadLine();
+                        break;
+                    }
+            }
+        }
+    }
+}
--- Demon/Truck/Truck/Program.cs
+++ Demon/Truck/Truck/Program.cs
@@ -1,65 +1,56 @@
-﻿using System;
+﻿// Swap two references. 
 
-namespace Truck
+using System;
+
+class RefSwap
 {
-	class Truck
-	{
-		public string Model;            //vehicle model
-		public double Length;           //vehicle length
-		public double Height;           //vehicle height
-		public double Width;            //vehicle width
-		public int MaxSpeed;            //vehicle max speed
-		public int Power;               //vehicle engine power
-		public int NumberOfGears;       //transmission number of gears
+    int a, b;
 
-		public Truck()
-		{
-			Model = "ZIL 157  ";
-			Length = 6.68;
-			Height = 2.9;
-			Width = 2.32;
-			MaxSpeed = 65;
-			Power = 109;
-			NumberOfGears = 5;       //All vehicles have 5-speed manual transmission
-		}
+    public RefSwap(int i, int j)
+    {
+        a = i;
+        b = j;
+    }
 
-		public Truck(string model, double length, double e, double r, int t, int y, int u)       //another vehicle parameters 
-		{
-			Model = model;
-			Length = length;
-			Height = e;
-			Width = r;
-			MaxSpeed = t;
-			Power = y;
-			NumberOfGears = u;
-		}
+    public void Show()
+    {
+        Console.WriteLine("a: {0}, b: {1}", a, b);
+    }
 
-		public double VehicleVolume()            //Vehicle volume
-		{
-			return Length * Height * Width;
-		}
-	}
-	class TruckDemo
-	{
-		static void Main()
-		{
-			var truckArray = new Truck[]
-			{
-				new Truck("URAL 4320", 7.4, 2.5, 3, 85, 240, 5),
-				new Truck("URAL 375D", 7.35, 2.9, 2.69, 75, 180, 5),
-				new Truck("URAL 375D", 7.35, 2.9, 2.69, 75, 180, 5),
-				new Truck("KRAZ 255B", 8.65, 3.18, 2.75, 71, 240, 5),
-				new Truck("KAMAZ 4310", 7.65, 3.59, 2.5, 85, 220, 5),
-				new Truck()
-		};
+    // This method changes its arguments. 
+    public void Swap(RefSwap ob1, RefSwap ob2)
+    {
+        RefSwap t;
 
-			Console.WriteLine("Model   \tLength\tHeight\tWidth\tMaxSp\tPower\tNuOfG\tVehicle Volume");
-			foreach (var truck in truckArray)
-			{
-				Console.Write(truck.Model + "\t" + truck.Length + "m\t" + truck.Height + "m\t" +
-				              truck.Width + "m\t" + truck.MaxSpeed + "km/h\t" + truck.Power + "hp\t" +
-				              truck.NumberOfGears + "\t" + truck.VehicleVolume() + "m^3\t\n\n");
-			}
-		}
-	}
+        t = ob1;
+        ob1 = ob2;
+        ob2 = t;
+    }
+}
+
+class RefSwapDemo
+{
+    static void Main()
+    {
+        RefSwap x = new RefSwap(1, 2);
+        RefSwap y = new RefSwap(3, 4);
+
+        Console.Write("x before call: ");
+        x.Show();
+
+        Console.Write("y before call: ");
+        y.Show();
+
+        Console.WriteLine();
+
+        // Exchange the objects to which x and y refer. 
+        x.Swap(x, y);
+
+        Console.Write("x after call: ");
+        x.Show();
+
+        Console.Write("y after call: ");
+        y.Show();
+
+    }
 }
