a8c8a0dd9febc95bfceb6d5b76e36e10dd955ac1
Aleksandr Zvetkov <seel.ru@gmail.com>
2017-02-28 23:07:22 +0300

    FizzBuzz

---
+++ Seel/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        static void Main()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 15 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
