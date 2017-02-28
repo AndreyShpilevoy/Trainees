94ec814ce8da83856320153456047d33177a795d
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-02-28 22:46:42 +0200

    Refactored

--- Seel/FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ Seel/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -2,32 +2,32 @@
 
 namespace FizzBuzz
 {
-    class FizzBuzz
-    {
-        static void Main()
-        {
-            for (var i = 1; i <= 100; i++)
-            {
-                if (i % 3 == 0)
-                {
-                    if (i % 15 == 0)
-                    {
-                        Console.WriteLine("FizzBuzz");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
-            }
-        }
-    }
+	class FizzBuzz
+	{
+		static void Main()
+		{
+			for (var i = 1; i <= 100; i++)
+			{
+				Console.WriteLine(GetConditionResult(i));
+			}
+		}
+
+		private static string GetConditionResult(int i)
+		{
+			string result;
+			if (i % 3 == 0)
+			{
+				result = i % 15 == 0 ? "FizzBuzz" : "Fizz";
+			}
+			else if (i % 5 == 0)
+			{
+				result = "Buzz";
+			}
+			else
+			{
+				result = i.ToString();
+			}
+			return result;
+		}
+	}
 }
