bf5fb144ee0dc3b8377190431cb7b92d0ca7c7b2
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-03-07 00:13:35 +0200

    added System.IO.File example

---
+++ Examples/FileReadAndWrite/FileReadAndWrite/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace FileReadAndWrite
+{
+	class Program
+	{
+		static void Main()
+		{
+			var pathToFile = @"C:\Git\Trainees\Examples\FileReadAndWrite\FileReadAndWrite\PhoneNumbers.txt";
+			var fileContent = File.ReadAllText(pathToFile);
+			fileContent = fileContent.Replace(":", "-");
+			File.WriteAllText(pathToFile, fileContent);
+		}
+	}
+}
